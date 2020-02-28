ccf4c9fa9f299f4d8a714be966b506634f52e766
Mikhov Arthur <32838010+paradise999@users.noreply.github.com>
2020-02-28 23:01:18 +0200

    Add files via upload

---
+++ Optimization.ps1
@@ -0,0 +1,27 @@
+$b = $true
+$Out = "Text"
+C:\Windows\Microsoft.NET\Framework64\v2.0.50727\ngen.exe executequeueditems
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\ngen.exe executequeueditems
+C:\Windows\Microsoft.NET\Framework\v2.0.50727\ngen.exe executequeueditems
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe executequeueditems
+$Out = DISM /Online /Cleanup-Image /ScanHealth | Out-String
+$Out = $Out.Remove(0, $Out.Length - 79)
+$Out
+$Out = $Out -split" "
+if ($Out -contains "успешно") {
+   Write-Output "Done"
+}
+else {
+   Write-Output "There is problem. Restore? (y/n)"
+   $ex=read-host 
+    if($ex -eq "y"){
+     Write-Output "Start Restore"	 
+     DISM /Online /Cleanup-Image /RestoreHealth | Out-Null
+     Write-Output "Restore"
+    }
+	else{
+	 Write-Output "Ignore Restore"
+         $b = $false
+	 }
+}
+if($b) {sfc /scannow}
