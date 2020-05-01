5d2f3be9e0aa8567d0c1295c76aa692900cb20e0
Mikhov Arthur <32838010+paradise999@users.noreply.github.com>
2020-05-01 17:04:33 +0300

    Add files via upload

--- Optimization.ps1
+++ Optimization.ps1
@@ -1,6 +1,4 @@
-$b = $true
-$Out = "Text"
-Write-Output "Optimize .NET?"
+Write-Output "`n Optimize .NET?"
 $ex = read-host
 if($ex -eq "y"){
 C:\Windows\Microsoft.NET\Framework64\v2.0.50727\ngen.exe executequeueditems
@@ -8,28 +6,21 @@ C:\Windows\Microsoft.NET\Framework64\v4.0.30319\ngen.exe executequeueditems
 C:\Windows\Microsoft.NET\Framework\v2.0.50727\ngen.exe executequeueditems
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe executequeueditems
 }
-Write-Output "Scan Health DISM?"
+Write-Output "`n Scan Health DISM? (y/n)"
 $ex = read-host
 if($ex -eq "y"){
- $Out = DISM /Online /Cleanup-Image /ScanHealth | Out-String
- $Out = $Out.Remove(0, $Out.Length - 79)
- $Out
- $Out = $Out -split" "
- if ($Out -contains "успешно") {
-   Write-Output "Done"
- }
- else {
-  Write-Output "There is problem. Restore? (y/n)"
-  $ex = read-host 
-  if($ex -eq "y"){
-   Write-Output "Start Restore"	 
-   DISM /Online /Cleanup-Image /RestoreHealth | Out-Null
-   Write-Output "Restore"
+DISM /Online /Cleanup-Image /ScanHealth
+ Write-Output "`n There is problem? Restore? (y/n)"
+ $ex = read-host
+ if($ex -eq "y"){
+  DISM /Online /Cleanup-Image /RestoreHealth | Out-Null
   }
-  else{
-   Write-Output "Ignore Restore"
-   $b = $false
  }
+ else{
+  Write-Output "`n Ignore Restore"
 }
- if($b) {sfc /scannow}
-}
+Write-Output "`n Scan now? (y/n)"
+$ex = read-host
+if($ex -eq "y"){
+ sfc /scannow
+ }
